6b37b68c8497197648e95c92f6a911cd88125d8b
cnggs_ <snapoleon@fairplayer.co>
2023-06-20 15:07:20 +0200

    Add possibility to override the host used for cloning with ssh (if using multiple ssh key on your local computer)

--- gitlab_clone_group.py
+++ gitlab_clone_group.py
@@ -1,10 +1,10 @@
-import os
-
 import git
 import gitlab
+import os
+import re
 
 
-def clone_group(gitlab_host, gitlab_token, group_id, repositories_root_dir):
+def clone_group(gitlab_host, gitlab_token, group_id, repositories_root_dir, host_for_ssh):
     gl = gitlab.Gitlab(url=gitlab_host, private_token=gitlab_token)
     group = gl.groups.get(group_id)
     subgroups = group.subgroups.list()
@@ -12,12 +12,14 @@ def clone_group(gitlab_host, gitlab_token, group_id, repositories_root_dir):
     for subgroup in subgroups:
         subgroup_detail = gl.groups.get(subgroup.id)
         clone_group(gitlab_host=gitlab_host, gitlab_token=gitlab_token, group_id=subgroup.id,
-                    repositories_root_dir=f"{repositories_root_dir}/{subgroup_detail.name}/")
+                    repositories_root_dir=f"{repositories_root_dir}/{subgroup_detail.name}/", host_for_ssh=host_for_ssh)
     for project in projects:
         if not os.path.exists(repositories_root_dir):
             os.mkdir(f"{repositories_root_dir}")
         if not os.path.exists(os.path.join(repositories_root_dir, project.name.lower())):
-            git.Git(repositories_root_dir).clone(project.ssh_url_to_repo)
+            host_pattern = r'@(.*?):'
+            replaced_host_string = re.sub(host_pattern, f'@{host_for_ssh}:', project.ssh_url_to_repo, count=1)
+            git.Git(repositories_root_dir).clone(replaced_host_string)
 
 
 if __name__ == "__main__":
@@ -25,4 +27,5 @@ if __name__ == "__main__":
     gitlab_token = input("Gitlab token: ")
     group_id = input("Gitlab group_id to retrieve: ")
     repositories_root_dir = input("Repository root directory (default is current): ") or "."
-    clone_group(gitlab_host, gitlab_token, group_id, repositories_root_dir)
+    host_for_ssh = input("Gitlab host for ssh clone (default value is gitlab.com): ") or "gitlab.com"
+    clone_group(gitlab_host, gitlab_token, group_id, repositories_root_dir, host_for_ssh)
